54c16e22f007bd97215e92a1db9c285c0aa82a3b
Tadea Simunovic <tsimunoviczg@gmail.com>
2018-09-23 21:57:29 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,18 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/posts/new' do #loads new form
+    erb :new
+  end
 
+
+  post '/posts' do
+    post = Post.create(params)
+    redirect to '/posts'  #create a post
+  end
+
+  get '/posts' do
+    @posts = Post.all
+    erb :index       #loads show page
   end
 end
